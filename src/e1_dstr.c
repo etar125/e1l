d3a06c3d1f77b1b917126c88ed1774e71948aca8
etar125 <inniesharpsoftware@gmail.com>
2026-04-07 21:30:28 +0300

    Реализация функций d_addstr и d_addcstr
    Также в шапках всех файлов к нику etar125 добавлено Admanse.

--- src/e1_dstr.c
+++ src/e1_dstr.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2026 etar125
+Copyright (c) 2026 etar125 Admanse
 
 Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
@@ -64,4 +64,32 @@ int d_addch(dstr_t *str, char ch) {
     str->buffsize = size;
     str->pointer = i;
     return 0;
+}
+
+int d_addstr(dstr_t *str, str_t *add) {
+    if (!str || !add) { return 1; }
+    str_t a;
+    a.data = str->data;
+    a.size = str->pointer;
+    str_t res = join(&a, add, '', false);
+    if (!res.data) { return 1; }
+    free(str->data);
+    size_t newsize = str->buffsize;
+    while (newsize <= res.size) { newsize *= 2; }
+    str->buffsize = newsize;
+    str->pointer = res.size;
+    str->data = malloc(newsize + 1);
+    if (!str->data) { free(res.data); return 1; }
+    memcpy(str->data, res.data, res.size);
+    str->data[res.size] = '\0';
+    str->data[newsize] = '\0';
+    return 0;
+}
+
+int d_addcstr(dstr_t *str, char *add) {
+    if (!str || !add) { return 1; }
+    str a;
+    a.data = add;
+    a.size = strlen(add);
+    return d_addstr(str, &a);
 }
